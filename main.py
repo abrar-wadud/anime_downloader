f79d75e95d8b3bbf14b4891487a6b13f0054c086
Tawsif <tawsif7492@gmail.com>
2024-10-01 10:37:00 +0600

    fixing some typo and more rich implementation

--- main.py
+++ main.py
@@ -4,22 +4,23 @@ import time
 from tqdm import tqdm
 import os
 from rich import print
+from rich import box
+from rich.prompt import Prompt
+from rich.prompt import IntPrompt
 from rich.table import Table
-from rich.console import Console
 from rich.progress import track
 
-console = Console()
 base_url = "https://anitaku.pe"
 captcha_v3 = "03AFcWeA5Phyls8xv9gUYojSUiGR6tmA6CHcpFLjGx3WWSh7DZOnYTlP78j0QwLR7lfmGpmYFLgzCExJJnEMFy7jKwZdDFcf7s8Y9UXhaHsUvB95A6qCvTps_qcyPn6oieU2IoTymVwvQYcg7hK66ioEfzuvUlLUfGr8r47nnJzxe-geY6KfElW86yrcnjUVg2ChyJ0ROOYC2wz9K18DeLKqf7yt2w5VsCfLBqoqaIeiFInZ2U508MKsewFtnIRf6DarTfnRPdIa1gCmZwQixgfLIUd-rTaZZLI5Aaa33AnU18r-3x2qfp7MruJ5uB9k1SLgfD9R7pUz7si0UP67V6qPNjhdyS3UKQfoBC26KJbWHLGNZtKFPQw73AzSOqdugWdVjFJ3LQdDQHgqDE6iojJNq82-wBo2g9_sZpzI-9EBDzTYEBC-9UxZE-ZXA6TpxnLCX2YPj4ZKsWXYfMWATXHtmIxBLqpnzRoMHoKuajX8Kw15Sk7c2ih696PLJzeMsLXumVRjHJgambHD-CwOrwZnkDg5rjX8KikrfnFPTIzloFn1EeSdju6EEl4hXQDhWgtlarmKAh_iEvj4GNMBXjj8gXl_-84xSBXHHw3q6YyELh4o6TsS2y4tm3HdjdNWiMZCqMFgiWTdanUC8LFq6bjs8GlxTdmkWBjD46GtsL0TwgHCItF2Kge6lDULiJj2PEnbzMy7QiUWKPZGzpamMDA7iOfE9oA_JguOl-Pi5yQd_PYHGyhL-BWE7UN34cjKoPNERWO_0PxRO42UJj7zR8uUcwSznv9E2kNeiIXogQsIy3oxBckBBtPyDHXi5QQ5klg8oUVzRCD7Rg"
 
 def choosing_anime():
     global selected_title
-    anime_search = console.input("[blue bold]Anime: [/blue bold]")
+    anime_search = Prompt.ask("[cyan bold]Anime[/cyan bold]")
 
     anime_links = []
     anime_titles = []
-    table = Table()
-    table.add_column("[cyan]No.[/cyan]",style="cyan bold", justify="center")
+    table = Table(box=box.ROUNDED)
+    table.add_column("[blue_violet]No[/blue_violet]",style="blue_violet bold", justify="center")
     table.add_column("[yellow]Anime Name[/yellow]", style="yellow")
     table.add_column("[green]Released[/green]", style="green", justify="center")
 
@@ -48,7 +49,7 @@ def choosing_anime():
 
     if len(anime_links) > 0:
         print(table)
-        choose_anime = int(console.input("[blue bold]Choose an anime: [/blue bold]")) - 1
+        choose_anime = IntPrompt.ask("[cyan bold]Choose an anime[/cyan bold]") - 1
         selected_title = anime_titles[choose_anime]
         max_ep(anime_links[choose_anime])
     else:
@@ -67,8 +68,8 @@ def max_ep(choosed_anime):
 def download_id(anime_soup, ep_end):
     anime_id = anime_soup.find('input', id="movie_id").get('value')
     
-    starting_ep = int(console.input("[blue bold]Starting episode: [/blue bold]")) if ep_end != '1' else 1
-    ending_ep = int(console.input("[blue bold]Ending episode: [/blue bold]")) if ep_end != '1' else 1
+    starting_ep = IntPrompt.ask("[cyan bold]Starting episode[/cyan bold]") if ep_end != '1' else 1
+    ending_ep = IntPrompt.ask("[cyan bold]Ending episode[/cyan bold]") if ep_end != '1' else 1
     
     if starting_ep <= ending_ep:
         eps_links_page = requests.get(f"https://ajax.gogocdn.net/ajax/load-list-episode?ep_start={starting_ep}&ep_end={ending_ep}&id={anime_id}")
@@ -80,13 +81,13 @@ def download_id(anime_soup, ep_end):
     ep_links = eps_soup.find_all('a')
     ep_links.reverse()
     id_list = []
-    for i in track(ep_links, "[blue bold]Processing...[/blue bold]", transient=True):
+    for i in track(ep_links, "[cyan bold]Processing...[/cyan bold]", transient=True):
         ep_page = requests.get(f"{base_url}{i.get('href').strip()}")
         ep_soup = BeautifulSoup(ep_page.content, 'html.parser')
         download_link = ep_soup.find('li', class_="dowloads").a.get('href')
         id = download_link.split('id=')[1].split('&')[0]
         id_list.append(id)
-        time.sleep(1)
+        time.sleep(0.5)
     get_res(id_list, starting_ep, ending_ep)
 
 def get_res(id_list, ep_start, ep_end):
@@ -94,7 +95,7 @@ def get_res(id_list, ep_start, ep_end):
     mid_res_dict = {}
     high_res_dict = {}
     counter = ep_start - 1
-    for i in track(id_list, "[blue bold]Getting Links...[/blue bold]", transient=True):
+    for i in track(id_list, "[cyan bold]Getting Links...[/cyan bold]", transient=True):
         data = {
             "captcha_v3": f"{captcha_v3}",
             "id": f"{i}"
@@ -131,23 +132,24 @@ def download_file(url, filename):
         # Set the chunk size for the download
         chunk_size = 8192
         
+        modified_filename = filename.removeprefix(f"/home/tawsif/Videos/{selected_title}/")
         # Open the file for writing in binary mode
         with open(f"{filename}.mp4", 'wb') as f:
             # Initialize tqdm progress bar with the total file size and chunk size
-            with tqdm(total=total_size, unit='B', unit_scale=True, desc=f"{filename}.mp4".removeprefix(f"/home/tawsif/Videos/{selected_title}")) as pbar:
+            with tqdm(total=total_size, unit='B', unit_scale=True, desc=f"{modified_filename}.mp4") as pbar:
                 # Write the file in chunks and update the progress bar
                 for chunk in r.iter_content(chunk_size=chunk_size):
                     if chunk:
                         f.write(chunk)
                         pbar.update(len(chunk))
     
-    print(f"[green bold]Downloaded {filename}.mp4[/green bold]".removeprefix(f"/home/tawsif/Videos/{selected_title}"))
+    print(f"[green bold]Downloaded {modified_filename}.mp4[/green bold]")
 
 def pick_res(low_res, mid_res, high_res, ep_start, ep_end):
     ep_range = int(ep_end) - int(ep_start) + 1
     
-    table = Table()
-    table.add_column("[cyan]No[/cyan]",style="cyan bold", justify="center")
+    table = Table(box=box.ROUNDED)
+    table.add_column("[blue_violet]No[/blue_violet]",style="blue_violet bold", justify="right")
     table.add_column("[yellow]Resolutions[/yellow]", style="yellow bold", justify="center")
     if len(low_res) == ep_range:
         table.add_row("1","[yellow]360P[/yellow]")
@@ -161,7 +163,7 @@ def pick_res(low_res, mid_res, high_res, ep_start, ep_end):
     print(table)
     if os.path.exists(f'/home/tawsif/Videos/{selected_title}'):
         if len(low_res) == ep_range or len(mid_res) == ep_range or len(high_res) == ep_range:
-            choosing_res = console.input('[blue bold]Choose a resolution: [/blue bold]')
+            choosing_res = Prompt.ask('[cyan bold]Choose resolution: [/cyan bold]', choices=['1','2','3'])
             match choosing_res:
                 case '1':
                     for link, num in zip(low_res.values(), range(ep_start, ep_end + 1)):
@@ -178,7 +180,7 @@ def pick_res(low_res, mid_res, high_res, ep_start, ep_end):
     else:
         os.mkdir(f"/home/tawsif/Videos/{selected_title}")
         if len(low_res) == ep_range or len(mid_res) == ep_range or len(high_res) == ep_range:
-            choosing_res = console.input('[blue bold]Choose a resolution: [/blue bold]')
+            choosing_res = Prompt.ask('[cyan bold]Choose resolution: [/cyan bold]', choices=['1','2','3'])
             match choosing_res:
                 case '1':
                     for link, num in zip(low_res.values(), range(ep_start, ep_end + 1)):
